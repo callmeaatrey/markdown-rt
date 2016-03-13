53e170e2460e22d551c5b5b3fbb827a304b28655
Shikher Aatrey <shikher.a@gmail.com>
2016-03-13 18:11:12 +0530

    Configuring Redis to be used for heroku

--- app.js
+++ app.js
@@ -2,11 +2,23 @@ var express = require('express');
 var app = express();
 var connect = require('connect');
 var sharejs = require('share');
-require('redis');
+
+// Set up redis server
+var redisClient;
+console.log(process.env.REDISTOGO_URL);
+if (process.env.REDISTOGO_URL) {
+  	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  	redisClient = require("redis").createClient(rtg.port, rtg.hostname);
+  	redisClient.auth(rtg.auth.split(":")[1]);
+} 
+else {
+  	redisClient = require("redis").createClient();
+}
 
 var options = {
 	db: {
-		type: 'redis', // Specifying the data store
+		type: 'redis',
+		client: redisClient, // Specifying the data store
 	}
 };
 
