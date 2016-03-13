7a519f1be7754587354a9437e00817a24e8a9b90
Shikher Aatrey <shikher.a@gmail.com>
2016-03-13 11:45:50 +0530

    Adding eventListener for implementing tab funcationality

--- public/script.js
+++ public/script.js
@@ -1,7 +1,24 @@
 window.onload = function() {
     var converter = new showdown.Converter();
     var pad = document.getElementById('pad');
-    var markdownArea = document.getElementById('markdown');   
+    var markdownArea = document.getElementById('markdown');
+
+    pad.addEventListener('keydown', function(e) {
+        if(e.keyCode === 9) {
+            var start = this.selectionStart;
+            var end = this.selectionEnd;
+            var target = e.target;
+            var value = target.value;
+
+            target.value = value.substring(0, start)+
+                            '\t'+
+                            value.substring(end);
+
+            this.selectionStart = this.selectionEnd = start + 1;
+
+            e.preventDefault();
+        }
+    });
 
     var previousMarkdownValue;          
 
@@ -27,7 +44,13 @@ window.onload = function() {
 
     pad.addEventListener('input', convertTextAreaToMarkdown);
 
-    sharejs.open('home', 'text', function(error, doc) {
-        doc.attach_textarea(pad);
-    });
+    if(document.location.pathname.length > 1) {
+        var pathName = document.location.pathname.substring(1);
+        sharejs.open(pathName, 'text', function(error, doc) {
+            doc.attach_textarea(pad);
+            convertTextAreaToMarkdown();
+        });
+    }
+
+    convertTextAreaToMarkdown();
 };
